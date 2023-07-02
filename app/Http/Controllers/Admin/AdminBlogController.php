59221977293228035505cde23e6bcba548c31e1f
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2023-07-02 09:57:13 +0900

    42

--- app/Http/Controllers/Admin/AdminBlogController.php
+++ app/Http/Controllers/Admin/AdminBlogController.php
@@ -47,7 +47,7 @@ class AdminBlogController extends Controller
      */
     public function edit(string $id)
     {
-        $blog = Blog::find($id);
+        $blog = Blog::findOrFail($id);
         return view('admin.blogs.edit', ['blog' => $blog]);
     }
 
